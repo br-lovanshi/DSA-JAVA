bc4e40d1ef4d3ba62a965f8d7933b6298cc83de1
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2022-10-07 23:41:18 +0530

    Update Java String BufferedReader.java

--- boilerPlateCode/Java String BufferedReader.java
+++ boilerPlateCode/Java String BufferedReader.java
@@ -40,3 +40,58 @@ public class Solution {
         bufferedReader.close();
     }
 }
+
+
+
+
+
+
+
+
+import java.io.*;
+import java.util.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+    
+  public static void subset(int n,int arr[]){
+      
+
+    
+  }
+
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        
+      
+        
+            int n = Integer.parseInt(bf.readLine().trim());
+           
+                
+             
+        
+            int [] arr = Arrays.stream(bf.readLine().replaceAll("\\s+$", "").split(" ")).
+            mapToInt(Integer::parseInt).toArray();
+            
+          
+            
+            subset(n,arr);
+            
+        
+
+
+        //   List<Integer> list = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        //     .map(Integer::parseInt)
+        //     .collect(toList());
+            
+       
+         
+
+        bf.close();
+        
+        
+        }
+}
