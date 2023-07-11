9a1d84f024980934ec9605f687a979d5a01aaefb
Brajesh Lovanshi <brajeshlovanshi2001@gmail.com>
2023-07-11 13:40:52 +0000

    merge string

--- String/BasicString.java
+++ String/BasicString.java
@@ -3,6 +3,12 @@ import java.util.*;
 class BasicString {
 
 public static void main(Sting[] args){
-    System.out.println("Hellow world!");
+    // System.out.println("Hellow world!");
+
+    String str = "Brajesh";
+    String str2 = "Lovanshi";
+    String str3 = str + str2;
+
+    System.out.println(str3);   
 }
 }
