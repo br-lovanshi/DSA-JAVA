b0c8b7a042ae53f63b3918d7cc98567009848858
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-09-08 21:47:18 +0530

    sum of n natural

---
+++ Arrays/sumOfnNatural.java
@@ -0,0 +1,14 @@
+public class sumOfnNatural {
+
+    public static void main(String[] args){
+
+        int n = 5;
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+        
+    }
+}
