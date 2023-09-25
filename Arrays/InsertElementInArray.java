b46b6a54f85b7bde287766c5bd54ffd5db3cdd7e
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-09-25 08:04:46 +0000

    update

--- Arrays/InsertElementInArray.java
+++ Arrays/InsertElementInArray.java
@@ -1,6 +1,4 @@
 
-import java.util.Arrays;
-
 class InsertElementInArray {
     
     public static int[] insertElement(int[] nums, int indx, int ele){
@@ -10,14 +8,16 @@ class InsertElementInArray {
         
         int[] newArray = new int[len+1];
         
+        // add ele before indx
         for(int i= 0;i<indx; i++){
             
             newArray[i] = nums[i];
             
         }
-        
+        // add ele at the indx 
         newArray[indx] = ele;
         
+        // add ele after index 
         for(int i = indx; i<len; i++){
             newArray[i+1] = nums[i];
         }
@@ -28,7 +28,12 @@ class InsertElementInArray {
         
         int[] array = {1,2,3,5,6,7};
         
+        // calling the method 
         int[] output =  insertElement(array,3,4);
-        System.out.println(Arrays.toString(output));
+
+        for(int i : output){
+            System.out.print( i + " ");
+        }
+        System.out.println();
     }
 }
