e41d733b96616083a6d48b3c5e5deae9df767db3
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-09-26 13:05:01 +0000

    String input taking

---
+++ InputTaking/StringsV.java
@@ -0,0 +1,24 @@
+//Enter code here
+
+// https://oj.masaischool.com/contest/3467/problem/13
+import java.io.*;
+
+
+public class StringsV {
+    
+
+    	
+    
+    
+	public static void main(String[] args) throws IOException {
+   	 
+    	BufferedReader bf = new BufferedReader(new   InputStreamReader(System.in));
+   		System.out.print(bf.readLine() + " ");
+   	 	
+    	bf.close();
+	}
+
+}
+
+
+
