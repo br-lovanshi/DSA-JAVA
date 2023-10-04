ef97e8c296466bf0c4516552d34262eba3446f49
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-10-04 18:06:28 +0000

    mejority

--- Arrays/MejorityElement.java
+++ Arrays/MejorityElement.java
@@ -2,26 +2,30 @@
 import java.util.Map;
 import java.util.HashMap;
 
-public class MejorityElement{
+public class MejorityElement {
+// using hasmap we can find the elements and their occurence
+    public static void mejorityElementFind(int[] nums) {
 
-    public static void mejorityElementFind(int[] nums){
-        
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        // add nums ele in the map
+        for (int i = 0; i < nums.length; i++) {
 
-        for(int i = 0;i<nums.length;i++){
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i] + 1));
+            } else
+                map.put(nums[i], 1);
+        }
+        int max = 0;
+        // for(Map.entry(Integer, Integer) entry : map.entrySet()){
             
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]+1));
-            }
-            else map.put(nums[i],1);
         }
 
-        System.out.print(map.containsKey(1));
+         
     }
-    
+
     public static void main(String[] args) {
-        
-        int[] arr = {1,2,3,5,2,2,2,6,7};
+
+        int[] arr = { 1, 2, 3, 5, 2, 2, 22, 6, 7 };
         mejorityElementFind(arr);
 
     }
