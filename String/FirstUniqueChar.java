b3bf5d519ebe43b926bbca6f5e292e8044ba4ab7
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-10-31 17:53:19 +0000

    firstUniqChar

---
+++ String/FirstUniqueChar.java
@@ -0,0 +1,32 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class firstUniqChar {
+    public static int solution(String s) {
+       Set<Character> letters = new HashSet<>();
+       Set<Character> hasSet = new HashSet<>();
+
+       for(char i : s.toCharArray()){
+           letters.add(i);
+
+           if(hasSet.contains(i)) letters.remove(i);
+           hasSet.add(i);
+
+       }
+           if(hasSet.isEmpty()) return -1;
+           int i = 0;
+
+           for(char c: s.toCharArray()){
+               if(letters.contains(c)) return i;
+               i++;
+           }
+
+           return -1;
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int ans = solution(str);
+        System.out.println(ans);
+    }
+}
