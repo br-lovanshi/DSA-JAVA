30c5deecbe8204c49147fb583eadd7e4007cbbbc
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-11-29 05:43:15 +0000

    integer break

---
+++ Number/IntegerBreak.java
@@ -0,0 +1,17 @@
+public class IntegerBreak {
+    public static int intBreak(int n){
+        if(n <2) return -1;
+
+        if(n %3 == 0){
+            int k = n/3;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int n = 2;
+        int mod = 2% 3;
+        int divide = 2/3;
+        System.out.println(mod + " " + divide + " " + (int) Math.pow(3,divide)*2);
+        // int ans = intBreak(n);
+    }
+}
