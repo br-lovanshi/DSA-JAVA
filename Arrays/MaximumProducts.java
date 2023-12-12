f8d861c32b9f2fa4499161b095b3a704c191a30a
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-12-12 16:09:16 +0000

    maximum product

---
+++ Arrays/MaximumProducts.java
@@ -0,0 +1,24 @@
+public class MaximumProducts {
+    public static int maxProduct(int[] nums, int n) {
+        int max1 = Integer.MIN_VALUE;// 4,4, 5
+        int max2 = Integer.MIN_VALUE;// 4, 4
+        for (int num : nums) {// 4, 4, 5
+            if (num >= max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+
+    public static void main(String[] args) {
+
+
+        int[] array = { 1, 2, 6, 3, 7, 3 };
+ 
+        System.out.println(maxProduct(array, array.length));
+    }
+}
