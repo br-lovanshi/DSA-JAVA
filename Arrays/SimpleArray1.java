78fbe9e83684f5dd89069ca55861c8852b6f5f29
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2024-01-17 17:07:33 +0000

    array

---
+++ Arrays/SimpleArray1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SimpleArray1 {
+    public static void main(String[] args) {
+        
+        int[] array = new int[5];
+        array[0] = 1;
+        array[2] = 4;
+        
+        boolean[] booleanArray = new boolean[4];
+        booleanArray[2] = true;
+        for(boolean i : booleanArray){
+            if(i == true){
+                System.out.println("Hello Wolrd!");
+            }
+        }
+        
+        int num = 10;
+        String str = "Hello";
+        Character ch = 'c';
+        Long l = 39349384938l;
+        double d = 4.4;
+        System.out.println(l);
+        
+    }
+}
