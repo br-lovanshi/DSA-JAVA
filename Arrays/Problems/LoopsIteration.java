011632b03929a54cc8af1e549a1a227a215fdf21
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2024-02-06 04:29:20 +0000

    Add insideproblems

---
+++ Arrays/Problems/LoopsIteration.java
@@ -0,0 +1,33 @@
+package Problems;
+public class LoopsIteration {
+ public static void main(String[] args) {
+
+    int len = 5;
+
+    for(int i = 0;i<len;i++){
+        for(int j = 0;j<=i;j++){
+            
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+    }
+
+    for(int i = len-1; i>= 0;i--){
+        for(int j = i;j<=len;j++){
+            System.out.println("* ");
+        }
+        System.out.println(" ");
+    }
+
+    int n = 5;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            if(i == 0 || i == n-1 || j == 0 || j == n-1)
+             System.out.print("*");
+             else{
+            System.out.print(" ");}
+        }
+        System.out.println();
+    }
+ }
+}
---
+++ Arrays/Problems/palindrome.java
@@ -0,0 +1,14 @@
+package Problems;
+class Palindrome {
+    public boolean isPalindrome(int x) {
+        
+        String str1 = Integer.toString(x);
+        String str2 = "";
+
+        for(int i = str1.length()-1;i>=0;i--){
+            str2 += str1.charAt(i);
+        }
+
+        return str1.equals(str2);
+    }
+}
