201268e123120516f1406d547bb5a325d8c77dbe
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2024-02-07 08:45:16 +0000

    most frequent

--- Arrays/MostFrequentElement.java
+++ Arrays/MostFrequentElement.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,11 +47,37 @@ public class MostFrequentElement {
 
         System.out.println("Max ele: " + maxEle);
     }
+
+    // if array is sorted or sort the array first
+    public static void findFrequentElement1(int[] arr){
+        Arrays.sort(arr);
+
+        int frequencyCount = 1;
+        int ele = arr[0];
+
+        int count = 0;
+        for(int i = 1; i<arr.length; i++){
+
+            if(arr[i] == arr[i-1]){
+                count++;
+            }else{
+                count = 1;
+            }
+
+            if(count > frequencyCount){
+                frequencyCount = count;
+                ele = arr[i];
+            }
+        }
+        System.out.println("Frequent Ele " + ele );
+
+    }
     public static void main(String[] args) {
         
         int arr[] = {1, 3, 2, 1,4,4,4,4, 4, 1};
         
         findFrequentElement(arr);// time o(n^2), space o(1)
+        findFrequentElement1(arr); // time o(n log n), space o(1)
         findEle(arr); // time o(n), space o(n)
 
     }
