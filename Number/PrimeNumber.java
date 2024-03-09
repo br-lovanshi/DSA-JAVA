7e54dd41087aa1bb9c521f3c8732ff1f1df598c4
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2024-03-09 07:27:52 +0000

    prime check

--- Number/PrimeNumber.java
+++ Number/PrimeNumber.java
@@ -1,20 +1,33 @@
 package Number;
 
 public class PrimeNumber {
-    public static boolean isPrime(int num){
+    // public static boolean isPrime(int num){
 
-        if(num <2) return false;
+        // if(num <2) return false;
 
-        for(int i = 2;i<=Math.sqrt(num);i++){
-            if(num%i == 0) return false;
-        }
+    //     for(int i = 2;i<=Math.sqrt(num);i++){
+    //         if(num%i == 0) return false;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
     public static void main(String[] args) {
         
         int num = 13;
         boolean isPrime = isPrime(num);
         System.out.println(isPrime);
+       
+    }
+
+    public static boolean isPrime(int num){
+
+        if(num < 2) return false;
+
+        for(int i = 2; i<=Math.sqrt(num);i++){
+            if(num%i == 0) return false;
+        }
+        
+        return true;
     }
 }
+
