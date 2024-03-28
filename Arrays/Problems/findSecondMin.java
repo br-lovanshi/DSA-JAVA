f0e59aa6a31171d52f91d5f55aa4f593d87be8f3
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-03-28 08:23:10 +0530

    dsa problems

---
+++ Arrays/Problems/findSecondMin.java
@@ -0,0 +1,25 @@
+package Problems;
+
+public class findSecondMin{
+
+	public static int findSecondMinNumber(int[] nums){
+		int firstMin = Integer.MAX_VALUE;
+		int secondMin = Integer.MAX_VALUE;
+		
+		for( int i = 0; i<nums.length; i++){
+			if(firstMin > nums[i]){
+				secondMin = firstMin;
+				firstMin = nums[i];
+			}
+			else if(secondMin > nums[i]){
+				secondMin = nums[i];
+			}
+		}
+		return secondMin;
+	}
+	public static void main(String args[]){
+		int[] nums = {2,5,1,2,0,6,7};
+		int output = findSecondMinNumber(nums);
+		System.out.println(output);
+	}
+}
