57e8b8d1d76fbd51621f90d6989c831091eca677
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-04-13 23:51:00 +0530

    binary search

--- Algorithms/BinarySearch/Problems/FirstGreaterELeOfTarget.java
+++ Algorithms/BinarySearch/Problems/FirstGreaterELeOfTarget.java
@@ -5,7 +5,7 @@ public class FirstGreaterELeOfTarget {
 
         int[] arr = {3,4,5,6,7,7,7,7,8,9,24,50};
         int target = 7;
-        int output = arch(abinarySerr,target);
+        int output = binarySearch(arr,target);
         System.out.println(output);
     }
 
