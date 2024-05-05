9c2ea6ae7a3463dbcade78ab22f1bf934ae07be7
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-05-05 23:12:08 +0530

    binary search

--- Number/PrimeNumberCheck.java
+++ Number/PrimeNumberCheck.java
@@ -1,2 +1,18 @@
-package Number;public class PrimeNumberCheck {
+package Number;
+
+public class PrimeNumberCheck {
+
+    public static void main(String[] args) {
+
+        int n = 25;
+
+        for(int i = 2; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+                System.out.println("Not Prime");
+            }else{
+                System.out.println("prime");
+            }
+        }
+//        System.out.println(Math.sqrt(n));
+    }
 }
