471c6c708f2099cfde68236b410f8cadfdec12da
Brajesh Lovanshi <brajesh.lovanshi@lendingkart.com>
2025-03-24 11:06:36 +0530

    second min & max ele

--- Arrays/Problems/findSecondMin.java
+++ Arrays/Problems/findSecondMin.java
@@ -1,5 +1,7 @@
 package Problems;
 
+import java.security.PublicKey;
+
 public class findSecondMin{
 
 	public static int findSecondMinNumber(int[] nums){
@@ -22,4 +24,23 @@ public class findSecondMin{
 		int output = findSecondMinNumber(nums);
 		System.out.println(output);
 	}
+
+	public static int secondMin(int[] nums){
+		int firstMin = Integer.MAX_VALUE;
+		int secondMin = Integer.MAX_VALUE;
+
+		for(int i = 0; i<nums.length; i++){
+			if(nums[i] > firstMin && nums[i] < secondMin){
+				secondMin = nums[i];
+			}
+			if(nums[i] < firstMin){
+				firstMin = nums[i];
+				secondMin = firstMin;
+			}
+		}
+
+		return secondMin;
+	}
 }
+
+//https://www.naukri.com/code360/problems/ninja-and-the-second-order-elements_6581960?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse&leftPanelTabValue=SUBMISSION
